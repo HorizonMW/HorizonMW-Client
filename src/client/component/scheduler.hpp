c4e28fe544446098ea212ef0da6180cd82825199
0x90sh <hi@0x90.sh>
2024-09-21 12:27:09 +0200

    feat(scheduler): Introduce the "network" pipeline & refactor functions to utilize it
    - Introduced a new "network" pipeline to handle I/O-bound tasks related to web requests related to the HMW Client and its network operations.
    - Adjusted several functions to offload IO bound tasks to the network pipeline, reducing main thread load/freezing/locking.
    - Network pipeline operates with a base delay of 50ms to reduce thread wakeups and idle resource usage.
    - Refactored task scheduling logic to make the most of this new asynchronous flow.
    
    Note: Because we all know the main thread deserves a break from answering all those pesky web requests. 🕸️ Let's give it some breathing room.

--- src/client/component/scheduler.hpp
+++ src/client/component/scheduler.hpp
@@ -1,11 +1,37 @@
 #pragma once
 
+/*
+	Author of comment: 0x90
+	Title: HMW/H2-Mod Scheduling System
+
+	Description:
+	Choosing the optimal pipeline/thread for the different tasks is cruicial for a good / smooth user experience. 
+	Asynchronous in this context means pipelines/threads completly unrelated to the game threads. We should call them HMW client threads.
+	The base delay of any pipeline is 10ms if not manually adjusted.
+
+	Guidelines for use:
+	1. Always assign tasks to the appropriate pipeline to maintain smooth game performance and avoid bottlenecks.
+	2. Tasks related to the game should use the appropriate game related thread.
+		- renderer -> The game's rendering pipeline
+		- server -> The game's server thread
+		- main -> The game's main thread
+		- lui -> For the lui context
+	3. Anything unrelated to the actual game which is loaded within memory we should definetly use our HMW client threads/pipelines.
+		- async -> Asynchronuous pipeline, disconnected from the game, our main thread for the HMW client
+		- network -> HMW client network related pipeline, mainly for IO bound tasks related to web requests (base delay of 50ms instead of 10ms)
+	4. For HMW internal dev's anticheat related pipelines do not use for anything else, timing matters alot there
+*/
+
 namespace scheduler
 {
 	enum pipeline
 	{
 		// Asynchronuous pipeline, disconnected from the game
 		async = 0,
+
+		//network related pipeline, mainly for IO bound tasks related to web requests
+		//base delay of 50ms instead of 10ms, to reduce thread idle resource usage
+		network,
 
 		// The game's rendering pipeline
 		renderer,
